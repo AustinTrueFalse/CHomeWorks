981b6fa07a5fefb9c4f5498bfd9874d77b07b9f0
Maksim <craash00077@gmail.com>
2023-11-06 14:02:50 +0300

    9.6.2 (HW-03)

--- CHomeWorks/Program.cs
+++ CHomeWorks/Program.cs
@@ -1,48 +1,90 @@
 ﻿using System;
 using System.IO;
+using System.Reflection.Metadata;
 using System.Security.Cryptography.X509Certificates;
 
-class MyException : Exception
-{
-    public MyException(string message) : base(message) { }
-}
 
 class Program
 {
+
+
     static void Main(string[] args)
     {
 
 
-        Exception[] ExceptionList = new Exception[5];
+        NumberReader numberReader = new NumberReader();
+        numberReader.NumberEnteredEvent += ShowNumber;
 
-        ExceptionList[0] = new FormatException();
-        ExceptionList[1] = new DivideByZeroException();
-        ExceptionList[2] = new OverflowException();
-        ExceptionList[3] = new DivideByZeroException();
-        ExceptionList[4] = new MyException("Число больше 10");
 
-        foreach (Exception i in ExceptionList)
+        while (true)
         {
             try
             {
-                int a = 10;
-                int b = 0;
-
-                Console.WriteLine(a / b);
-
-                throw i;
-
+                numberReader.Read();
 
             }
-            catch (Exception ex)
+            catch (MyException)
             {
-                Console.WriteLine(ex.Message);
-
+                Console.WriteLine("Введено некорректное значение");
             }
+
+
         }
 
+        static void ShowNumber(int number)
+        {
+
+            List<string> Users = new List<string>() { "Ivanov", "Petrov", "Erohin", "Sychov" };
+
+            switch (number)
+            {
+                case 1:
+                    Users.Sort();
+                    foreach (string User in Users)
+                    {
+                        Console.WriteLine(User);
+                    }; break;
+                case 2:
+                    Users.Sort();
+                    Users.Reverse();
+                    foreach (string User in Users)
+                    {
+                        Console.WriteLine(User);
+                    }; break;
+            }
+        }
     }
 
+    class NumberReader
+    {
+
+        public delegate void NumberEnteredDelegate(int value);
+        public event NumberEnteredDelegate NumberEnteredEvent;
+
+        public void Read()
+        {
+
+            Console.WriteLine("Отсортируйте список, нажмите 1 - отсортировать по возрастанию, 2 - отсортировать по убыванию");
+
+            int number = Convert.ToInt32(Console.ReadLine());
+
+            if (number != 1 && number != 2) throw new MyException("Некорректный формат");
+
+            NumberEntered(number);
+
+        }
+
+        public virtual void NumberEntered(int number)
+        {
+            NumberEnteredEvent?.Invoke(number);
+        }
+    }
+
+    class MyException : Exception
+    {
+        public MyException(string message)
+            : base(message) { }
+    }
 }
 
 
