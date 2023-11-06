ac0c15c3e7738773976cb3a1b3ba77f3265a0bcd
Maksim <craash00077@gmail.com>
2023-11-06 13:06:28 +0300

    Добавьте файлы проекта.

---
+++ CHomeWorks/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.IO;
+using System.Security.Cryptography.X509Certificates;
+
+class MyException : Exception
+{
+    public MyException(string message) : base(message) { }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+
+
+        Exception[] ExceptionList = new Exception[5];
+
+        ExceptionList[0] = new FormatException();
+        ExceptionList[1] = new DivideByZeroException();
+        ExceptionList[2] = new OverflowException();
+        ExceptionList[3] = new DivideByZeroException();
+        ExceptionList[4] = new MyException("Число больше 10");
+
+        foreach (Exception i in ExceptionList)
+        {
+            try
+            {
+                int a = 10;
+                int b = 0;
+
+                Console.WriteLine(a / b);
+
+                throw i;
+
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+
+            }
+        }
+
+    }
+
+
+}
+
+
+
+
+
+
