07c7d5ca714d3eac037d34bef2d87fa23719df4f
Maksim <craash00077@gmail.com>
2023-12-10 08:25:48 +0300

    Задание 13.6.1 (HW-03)

---
+++ CHomeWorks/ListTest.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace CHomeWorks
+{
+    public class ListTest
+    {
+        public static void ListTestMethod()
+        {
+            //Timer
+            var stopWatch = Stopwatch.StartNew();
+
+            //List
+            var listTest = new List<string>();
+
+            //ReadFile
+            string text = File.ReadAllText("C:\\Users\\craas\\Text.txt");
+            char[] delimiters = new char[] { ' ', '\r', '\n' };
+            var words = text.Split(delimiters, StringSplitOptions.RemoveEmptyEntries);
+
+            //ListInput
+            listTest.AddRange(words);
+
+            Console.WriteLine($"Время на создание массива из файла: {stopWatch.Elapsed.TotalMilliseconds}");           
+        }
+    }
+}
--- CHomeWorks/Program.cs
+++ CHomeWorks/Program.cs
@@ -1,84 +1,18 @@
-﻿using System;
-using System.IO;
-using System.Reflection.Metadata;
-using System.Security.Cryptography.X509Certificates;
-using static Program;
+﻿using CHomeWorks;
+using System;
+using System.Collections.Concurrent;
+using System.Threading;
+
 
 
 class Program
 {
-    public class User
+
+    public static void Main(string[] args)
     {
-        public string Login { get; set; }
-        public string Name { get; set; }
-        public bool IsPremium { get; set; }
-    }
-
-    static void Main(string[] args)
-    {
-        List<User> users = new List<User>
-        {
-            new User()
-            {
-                Login = "user1",
-                Name = "Peter",
-                IsPremium = true,
-            },
-            new User()
-            {
-                Login = "user2",
-                Name = "Alex",
-                IsPremium = false,
-            },
-            new User()
-            {
-                Login = "user3",
-                Name = "Jennifer",
-                IsPremium = false,
-            }
-        };
-        CheckUser(users);
-    }
-
-    static void CheckUser(List<User> users)
-    {
-        foreach(var user in users)
-        {
-            if (!user.IsPremium)
-            {
-                Greeting(user.Name);
-                ShowAds();
-            }
-            else
-            {
-                Greeting(user.Name);
-            }
-        }
-    }
-
-    static void Greeting(string name)
-    {
-        Console.WriteLine($"Привет, {name}!");
-    }
-
-    static void ShowAds()
-    {
-        Console.WriteLine("Посетите наш новый сайт с бесплатными играми free.games.for.a.fool.com");
-        // Остановка на 1 с
-        Thread.Sleep(1000);
-
-        Console.WriteLine("Купите подписку на МыКомбо и слушайте музыку везде и всегда.");
-        // Остановка на 2 с
-        Thread.Sleep(2000);
-
-        Console.WriteLine("Оформите премиум-подписку на наш сервис, чтобы не видеть рекламу.");
-        // Остановка на 3 с
-        Thread.Sleep(3000);
+        ListTest.ListTestMethod();
+        LinkedTest.LinkedTestMethod();
     }
 
 
 }
-
-
-
-
