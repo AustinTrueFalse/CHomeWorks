4790fd75803ee8cd59e894d0c7af0a4d3ace7a4f
Maksim <craash00077@gmail.com>
2023-12-23 12:35:54 +0300

    15.6.6. Практическое задание (HW-03)

--- CHomeWorks/Program.cs
+++ CHomeWorks/Program.cs
@@ -1,6 +1,8 @@
 ﻿
+using Microsoft.CodeAnalysis.CSharp;
 using System;
 using System.Collections.Concurrent;
+using System.Numerics;
 using System.Runtime.InteropServices;
 using System.Threading;
 
@@ -8,65 +10,57 @@ using System.Threading;
 
 class Program
 {
+
     static void Main(string[] args)
     {
-        //  создаём пустой список с типом данных Contact
-        var phoneBook = new List<Contact>();
-
-        // добавляем контакты
-        phoneBook.Add(new Contact("Игорь", "Николаев", 79990000001, "igor@example.com"));
-        phoneBook.Add(new Contact("Игорь", "Довлатов", 79990000010, "serge@example.com"));
-        phoneBook.Add(new Contact("Анатолий", "Карпов", 79990000011, "anatoly@example.com"));
-        phoneBook.Add(new Contact("Валерий", "Леонтьев", 79990000012, "valera@example.com"));
-        phoneBook.Add(new Contact("Сергей", "Брин", 799900000013, "serg@example.com"));
-        phoneBook.Add(new Contact("Иннокентий", "Смоктуновский", 799900000013, "innokentii@example.com"));
-
-   
-        while (true)
+        var classes = new[]
         {
-           
-            var input = Console.ReadKey().KeyChar;
+               new Classroom { Students = {"Evgeniy", "Sergey", "Andrew"}, },
+               new Classroom { Students = {"Anna", "Viktor", "Vladimir"}, },
+               new Classroom { Students = {"Bulat", "Alex", "Galina"}, }
+           };
 
 
-            var parsed = Int32.TryParse(input.ToString(), out int pageNumber);
+        var allStudents = GetAllStudents(classes);
 
 
-            if (!parsed || pageNumber < 1 || pageNumber > 3)
+        Console.WriteLine(string.Join(" ", allStudents));
+
+
+        //foreach (var classroom in classes)
+        //{
+        //    foreach (var student in classroom.Students)
+        //    {
+        //        Console.WriteLine(student);
+        //    }
+        //}
+    }
+
+
+
+
+    static string[] GetAllStudents(Classroom[] classes)
+    {
+        var result = new List<string>();
+
+        foreach (var classroom in classes)
+        {
+            foreach (var student in classroom.Students)
             {
-                Console.WriteLine();
-                Console.WriteLine("Страницы не существует");
-            }
-
-            else
-            {
-                var pageContent = phoneBook
-                    .Skip((pageNumber - 1) * 2)
-                    .Take(2)
-                    .OrderBy(p => p.Name)
-                    .ThenBy(p => p.LastName);
-
-                Console.WriteLine();
-
-                foreach (var entry in pageContent)
-                    Console.WriteLine(entry.Name + " " + entry.LastName + ": " + entry.PhoneNumber);
-
-                Console.WriteLine();
+                result.Add(student);
             }
         }
-    }
-}
-public class Contact // модель класса
-{
-    public Contact(string name, string lastName, long phoneNumber, String email) // метод-конструктор
-    {
-        Name = name;
-        LastName = lastName;
-        PhoneNumber = phoneNumber;
-        Email = email;
+
+        return result.ToArray();
+
     }
 
-    public String Name { get; }
-    public String LastName { get; }
-    public long PhoneNumber { get; }
-    public String Email { get; }
+    public class Classroom
+    {
+        public List<string> Students = new List<string>();
+    }
+
+
 }
+
+
